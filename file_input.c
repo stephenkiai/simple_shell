6545ce24bb6391fe4c8903656f4532068cb8321c
Twayfrancis <tawaifrancis@gmail.com>
2023-04-24 14:14:49 -0700

    file_input.c

---
+++ file_input.c
@@ -0,0 +1,47 @@
+#include "simple_shell.h"
+/**
+ * file_input - reads a file and execute the commands
+ * @filename: The name of the file to read
+ * Author Steve and Tway
+ * Return: 0 on success, 1 on failure
+ */
+
+int file_input(char *filename)
+{
+	FILE *fp = fopen(filename, "r");
+	char *line = NULL;
+	size_t buffer_size = 0;
+	int status = 0;
+
+	if (fp == NULL)
+	{
+		perror("file_input");
+		return (1);
+	}
+
+	while (getline(&line, &buffer_size, fp) != -1)
+	{
+		char **args = parse_line(line);
+
+		if (args[0] != NULL)
+		{
+			if (strcmp(args[0], "exit") == 0)
+			{
+				status = exit_builtin(args);
+
+			}
+			else
+			{
+				status = execute(args);
+			}
+		}
+		free(args);
+		if (status != 0)
+		{
+			break;
+		}
+	}
+	free(line);
+	fclose(fp);
+	return (status);
+}
