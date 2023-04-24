8a5ec62303e35cf0d1ef965a1e14cbf9b465dbbe
stephen kiai <stephenkiai23@gmail.com>
2023-04-25 00:43:38 +0300

    panic

---
+++ panic.c
@@ -0,0 +1,12 @@
+#include "simple_shell.h"
+/**
+ *panic - prints error msg & exits prgm with failure status
+ *@message: error message to be printed
+ *
+ */
+
+void panic(char *message)
+{
+	perror(message);
+	exit(EXIT_FAILURE);
+}
